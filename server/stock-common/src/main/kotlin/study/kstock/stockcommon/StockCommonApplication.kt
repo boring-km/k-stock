6c1c113afe8251eeae90088d6b2e1971e55dc207
boring-km <kms0644804@naver.com>
2021-03-09 13:01:04 +0900

    [KS-SER-01] stock-common 모듈 추가

---
+++ server/stock-common/src/main/kotlin/study/kstock/stockcommon/StockCommonApplication.kt
@@ -0,0 +1,11 @@
+package study.kstock.stockcommon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StockCommonApplication
+
+fun main(args: Array<String>) {
+	runApplication<StockCommonApplication>(*args)
+}
