c505de43e8eb079701e1f78dc6fa18121694c8ae
boring-km <kms0644804@naver.com>
2021-03-10 16:34:42 +0900

    [KS-SER-02] kafka를 이용해 stock-api에서 publish한 topic을 subscribe 해보자

---
+++ server/stock-core/src/main/kotlin/study/kstock/stockcore/service/PingService.kt
@@ -0,0 +1,21 @@
+package study.kstock.stockcore.service
+
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.support.Acknowledgment
+import org.springframework.stereotype.Service
+
+@Service
+class PingService {
+    @KafkaListener(topics = ["stock-api"], containerFactory = "pingKafkaListenerContainerFactory")
+    fun serviceListener(data: ConsumerRecord<String, Any>, ack: Acknowledgment) {
+        try {
+            val value = data.value()
+            println("received ping message: $value")
+            ack.acknowledge()
+        } catch (e: Exception) {
+            val msg = "시스템에 예상치 못한 문제가 발생했습니다"
+            println("received ping message: $msg$e")
+        }
+    }
+}
