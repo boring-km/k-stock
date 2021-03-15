b03b00dd13134edc39ce8ddacac0c3e18aa08288
boring-km <kms0644804@naver.com>
2021-03-15 14:52:21 +0900

    [KS-SER-03] Kafka 통신 코드 네이밍을 변경하고 코드 정리한다

---
+++ server/stock-api/src/main/kotlin/study/kstock/stockapi/domain/StockData.kt
@@ -0,0 +1,4 @@
+package study.kstock.stockapi.domain
+
+class StockData {
+}
--- server/stock-core/src/main/kotlin/study/kstock/stockcore/service/ReceiveService.kt
+++ server/stock-core/src/main/kotlin/study/kstock/stockcore/service/ReceiveService.kt
@@ -1,33 +1,28 @@
 package study.kstock.stockcore.service
 
 import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.slf4j.Logger
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Service
 
 @Service
-class PingService {
-    @KafkaListener(topics = ["stock-api"], containerFactory = "pingKafkaListenerContainerFactory")
-    fun apiServiceListener(data: ConsumerRecord<String, Any>, ack: Acknowledgment) {
-        try {
-            val value = data.value()
-            println("stock: $value")
-            ack.acknowledge()
-        } catch (e: Exception) {
-            val msg = "시스템에 예상치 못한 문제가 발생했습니다"
-            println("received ping message: $msg$e")
-        }
-    }
+class ReceiveService {
 
-    @KafkaListener(topics = ["stock-external"], containerFactory = "pingKafkaListenerContainerFactory")
-    fun externalServiceListener(data: ConsumerRecord<String, Any>, ack: Acknowledgment) {
+    private val logger: Logger = org.slf4j.LoggerFactory.getLogger(ReceiveService::class.java)
+
+    @KafkaListener(topics = ["stock-api", "stock-external"], containerFactory = "receiveKafkaListenerContainerFactory")
+    fun serviceListener(data: ConsumerRecord<String, Any>, ack: Acknowledgment) {
+        @Suppress("UNCHECKED_CAST")
         try {
-            val value = data.value()
-            println("received ping message: $value")
+            val value: LinkedHashMap<String, Any> = data.value() as LinkedHashMap<String, Any>
+            // TODO: 처리 업무가 필요한 service의 메서드 호출
+            //  1. 사용자 처리 요청 (stock-api)
+            //  2. 주식 데이터 저장 요청 (stock-external)
             ack.acknowledge()
         } catch (e: Exception) {
             val msg = "시스템에 예상치 못한 문제가 발생했습니다"
-            println("received ping message: $msg$e")
+            logger.error("received ping message: $msg$e")
         }
     }
 }
