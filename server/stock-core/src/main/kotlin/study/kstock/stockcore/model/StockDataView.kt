19101ca65acde521885ed6399c6a50a2134b70d7
boring-km <kms0644804@naver.com>
2021-03-20 22:39:07 +0900

    [KS-SER-05-km] StockData_View 추가
    아직 잘 동작하는 지 확인되지 않았으니 빨리 확인하자.

---
+++ server/stock-core/src/main/kotlin/study/kstock/stockcore/model/StockDataView.kt
@@ -0,0 +1,22 @@
+package study.kstock.stockcore.model
+
+
+import java.io.Serializable
+import javax.persistence.*
+
+@Entity
+@Table(name = "StockData_View")
+class StockDataView(
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private var id: Int,
+
+    @ManyToOne
+    @JoinColumn(name = "symbolId")
+    var stockSymbol: StockSymbol,
+
+    @Column var lastPrice: Double,
+    @Column var priceChange: Double,
+    @Column var percentChange: Double): Serializable {
+
+}
--- server/stock-core/src/main/kotlin/study/kstock/stockcore/model/StockRepository.kt
+++ server/stock-core/src/main/kotlin/study/kstock/stockcore/model/StockRepository.kt
@@ -2,15 +2,19 @@ package study.kstock.stockcore.model
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
-import java.util.*
 
 @Repository
 interface StockMarketRepository: JpaRepository<StockMarket, Int> {
-    fun findByMarketName(name: String): Optional<StockMarket>
+    fun findByRegion(country: String): MutableList<StockMarket>
 }
 
 @Repository
-interface StockDataRepository: JpaRepository<StockData, Int>
+interface StockDataRepository: JpaRepository<StockData, Int> {
+
+}
 
 @Repository
 interface StockSymbolRepository: JpaRepository<StockSymbol, Int>
+
+@Repository
+interface StockDataViewRepository: JpaRepository<StockDataView, Int>
