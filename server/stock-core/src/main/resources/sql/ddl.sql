292f84df634053f127de255c4312f9f6aa62ef4b
boring-km <kms0644804@naver.com>
2021-03-21 21:19:10 +0900

    [KS-SER-05-km] MariaDB DDL 및 기본 DML 작성

--- server/stock-core/src/main/resources/sql/ddl.sql
+++ server/stock-core/src/main/resources/sql/ddl.sql
@@ -2,14 +2,24 @@ create table StockMarket
 (
     id         int auto_increment primary key,
     marketName varchar(100),
-    country    varchar(100)
+    region     varchar(100)
 );
 
 create table StockSymbol
 (
-    id         int auto_increment primary key,
-    symbol     varchar(10),
-    name       varchar(100),
-    marketCode int,
-    foreign key (marketCode) references StockMarket (id) on update cascade
+    id       int auto_increment primary key,
+    symbol   varchar(10),
+    name     varchar(100),
+    marketId int,
+    foreign key (marketId) references StockMarket (id) on update cascade
+);
+
+create table StockData
+(
+    id            int auto_increment primary key,
+    symbolId      int,
+    lastPrice     double,
+    priceChange   double,
+    percentChange double,
+    foreign key (symbolId) references StockSymbol (id) on update cascade
 );
