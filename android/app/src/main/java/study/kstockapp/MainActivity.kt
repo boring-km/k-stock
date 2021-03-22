8bda0cc7ea4ae8dd83694750178ff3dc2ea7a6c7
KURIET <prospersj777@gmail.com>
2021-03-22 22:55:15 +0900

    [KS-APP-02] 주식정보를 표현할 수 있도록 해보자

--- android/app/src/main/java/study/kstockapp/MainActivity.kt
+++ android/app/src/main/java/study/kstockapp/MainActivity.kt
@@ -1,12 +1,18 @@
 package study.kstockapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import study.kstockapp.databinding.ActivityMainBinding
+import study.kstockapp.databinding.CardLayoutBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -48,7 +54,42 @@ class MainActivity : AppCompatActivity() {
             sampleStockData.add(StockData("주식데이터 $i 번째", "NYSE", 30.25,0.0,0.0))
         }
 
+        // 리사이클러 뷰에 LinearLayoutManager 및 adapter 적용
+        binding.recyclerviewSearchedStock.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = StockAdapter(sampleStockData) {
+                    stock ->
+                Toast.makeText(this@MainActivity, "$stock", Toast.LENGTH_SHORT).show()
+                val intent = Intent(baseContext, StockDetail::class.java)
+                intent.putExtra("obj", stock)
+                startActivity(intent)
+            }
+        }
+
     }
 
 }
 
+class StockAdapter(val items: List<StockData>,
+                   private val clickListener: (stockData: StockData) -> Unit) :
+    RecyclerView.Adapter<StockAdapter.StockViewHolder>() {
+
+    class StockViewHolder(val binding: CardLayoutBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StockViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.card_layout, parent, false)
+        val viewHolder = StockViewHolder(CardLayoutBinding.bind(view))
+        view.setOnClickListener {
+            clickListener.invoke(items[viewHolder.adapterPosition])
+        }
+        return viewHolder
+
+    }
+
+    override fun getItemCount() = items.size
+
+    override fun onBindViewHolder(holder: StockViewHolder, position: Int) {
+        holder.binding.stockdata = items[position]
+    }
+}
