3f296d80030e90ff2173fa7b9c9156106329ccee
KURIET <39091610+KURIET@users.noreply.github.com>
2021-03-27 21:19:46 +0900

    [KS-APP-02] 주식검색화면과 상세내역화면의 초안을 만들자 (#18)
    * [KS-APP-02] 시장에 따른 주식 검색 UI와 검색된 주식정보들이 나올 RecyclerView를 배치하자
    
    * [KS-APP-02] 데이터 바인딩을 사용해보자
    
    데이터 바인딩을 사용해 주식시장을 선택하는 spinner에 리스너를 달아보자.
    화면 내에서 CardView를 확인하기 위한 샘플데이터를 만들어보자.
    
    * [KS-APP-02] 주식정보를 표현할 수 있도록 해보자
    
    * [KS-APP-02] 주식정보의 세부내용을 볼 화면을 마련하자
    
    * fixup! [KS-APP-01] Android Project Initialization (#2)
    
    * [KS-APP-02] 안쓰는 코드는 주석으로 달지 말고 지우자
    
    * [KS-APP-02] 코드에 주석을 달 때는 링크를 달지 말자
    
    링크의 형태로 설명을 따로 붙이려 했지만
    onNothingSelected가 어떻게 동작하는지는 검색하면 나오기 때문에
    주석을 삭제하였다.
    
    * [KS-APP-02] 코드의 내용과 중복되는 주석을 적지 말자
    
    * [KS-APP-02] spinner item을 array.xml로 관리하지 말자
    
    아무리 테스트 코드라도 array.xml 같이 정적 형태의 데이터를 spinner item으로 쓰지는 말자.
    
    * [KS-APP-02] StockAdapter 클래스를 별도 파일로 만들어 관리하자
    
    * [KS-APP-02] 앱 시작 시 EditText 자동 포커스를 막자
    
    앱이 켜지자마자 EditText에 포커싱이 되며 자동으로 키보드가 올라오는 것을 막도록 상위 뷰에 포커싱을 하게 만들었다.
    
    Co-authored-by: KURIET <prospersj777@gmail.com>
    Co-authored-by: LSJ <sjlee@qubicom.co.kr>

--- android/app/src/main/java/study/kstockapp/MainActivity.kt
+++ android/app/src/main/java/study/kstockapp/MainActivity.kt
@@ -1,11 +1,71 @@
 package study.kstockapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import study.kstockapp.databinding.ActivityMainBinding
+import study.kstockapp.databinding.CardLayoutBinding
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        private const val TAG = "MainActivity"
+    }
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.main = this
+
+        val testStockItem = arrayOf("NYSE", "NASDAQ", "AMEX")
+        binding.spinnerStockMarket.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, testStockItem)
+        binding.spinnerStockMarket.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                Toast.makeText(this@MainActivity, "시장 중 하나를 선택해주세요", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val bindViewText =
+                    "You pressed position $position, Market ${binding.spinnerStockMarket.selectedItem}"
+                binding.textView.text = bindViewText
+            }
+        }
+
+        // 샘플 데이터 삽입
+        val sampleStockData = arrayListOf<StockData>()
+        for (i in 1..6) {
+            sampleStockData.add(StockData("주식데이터 $i 번째", "NYSE", 30.25,0.0,0.0))
+        }
+
+        binding.recyclerviewSearchedStock.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = StockAdapter(sampleStockData) {
+                    stock ->
+                Toast.makeText(this@MainActivity, "$stock", Toast.LENGTH_SHORT).show()
+                val intent = Intent(baseContext, StockDetail::class.java)
+                intent.putExtra("obj", stock)
+                startActivity(intent)
+            }
+        }
+
     }
+
 }
