cb6fddb30127dd5ad0e8b6536f982010db2c1985
KURIET <prospersj777@gmail.com>
2021-04-10 15:41:59 +0900

    [KS-APP-03] 빠른 파악을 위한 주석 추가

--- android/app/src/main/java/study/kstockapp/network/KStockService.kt
+++ android/app/src/main/java/study/kstockapp/network/KStockService.kt
@@ -6,12 +6,15 @@ import retrofit2.http.Path
 import study.kstockapp.domain.StockData
 
 interface KStockService {
+    // 주식 데이터 주어진 인덱스로부터 20개씩 조회
     @GET("stock/list/{market}/{index}")
     fun getStockListByMarketWithIndex(@Path("market") marketString : String, @Path("index") index: Int) : Call<List<StockData>>
 
+    // Region에 따른 거래소 목록 조회
     @GET("stock/market/list/{region}")
     fun getStockSymbolListByNation(@Path("region") regionString : String) : Call<StockData>
 
+    // Symbol로 주식 조회
     @GET("search/{symbol}")
     fun getStockBySymbol(@Path("symbol") symbolString : String) : Call<StockData>
 }
