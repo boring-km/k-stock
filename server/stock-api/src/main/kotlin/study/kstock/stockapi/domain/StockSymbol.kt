600b2472535ae4bbf69faad02ccf6a9618c61b0d
boring-km <kms0644804@naver.com>
2021-04-18 00:45:06 +0900

    [KS-SER-08-km] StockData 객체를 Deserialize 할 수 있게 사전 작업을 한다

--- server/stock-api/src/main/kotlin/study/kstock/stockapi/domain/StockSymbol.kt
+++ server/stock-api/src/main/kotlin/study/kstock/stockapi/domain/StockSymbol.kt
@@ -1,3 +1,11 @@
 package study.kstock.stockapi.domain
 
-data class StockSymbol(var symbol: String, var name: String, var stockMarket: StockMarket)
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonTypeInfo
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS)
+class StockSymbol @JsonCreator constructor(
+    @JsonProperty("symbol") var symbol: String,
+    @JsonProperty("name") var name: String,
+    @JsonProperty("stockMarket") var stockMarket: StockMarket)
