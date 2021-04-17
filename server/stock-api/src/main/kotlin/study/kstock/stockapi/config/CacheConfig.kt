e49863500a96fa56cf94cb16a82f2c7b359dc3ca
boring-km <kms0644804@naver.com>
2021-04-17 21:37:05 +0900

    [KS-SER-08-km] stock-core의 redis cache config 설정을 복사해오자

---
+++ server/stock-api/src/main/kotlin/study/kstock/stockapi/config/CacheConfig.kt
@@ -0,0 +1,36 @@
+package study.kstock.stockapi.config
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.cache.CacheManager
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.cache.RedisCacheConfiguration
+import org.springframework.data.redis.cache.RedisCacheManager
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.RedisSerializationContext
+import org.springframework.data.redis.serializer.StringRedisSerializer
+import java.time.Duration
+
+@Configuration
+class CacheConfig {
+
+    @Autowired
+    private lateinit var connectionFactory: RedisConnectionFactory
+
+    @Bean
+    fun redisCacheManager(): CacheManager {
+        val redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+            .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(StringRedisSerializer()))
+            .serializeValuesWith(
+                RedisSerializationContext.SerializationPair.fromSerializer(
+                    GenericJackson2JsonRedisSerializer()
+                )
+            )
+            .entryTtl(Duration.ofSeconds(30))
+        return RedisCacheManager.RedisCacheManagerBuilder
+            .fromConnectionFactory(connectionFactory)
+            .cacheDefaults(redisCacheConfiguration)
+            .build()
+    }
+}
