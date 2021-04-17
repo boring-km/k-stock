2594efcd383de28e2d15bfe7113cb9f30f894542
boring-km <kms0644804@naver.com>
2021-04-18 00:45:56 +0900

    [KS-SER-08-km] Redis Cache 설정 변경
    stock-core 서버에도 동일하게 변경 필요하다.

--- server/stock-api/src/main/kotlin/study/kstock/stockapi/StockApiApplication.kt
+++ server/stock-api/src/main/kotlin/study/kstock/stockapi/StockApiApplication.kt
@@ -2,9 +2,7 @@ package study.kstock.stockapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cache.annotation.EnableCaching
 
-@EnableCaching
 @SpringBootApplication
 class StockApiApplication
 
--- server/stock-api/src/main/kotlin/study/kstock/stockapi/config/CacheConfig.kt
+++ server/stock-api/src/main/kotlin/study/kstock/stockapi/config/CacheConfig.kt
@@ -1,36 +1,33 @@
 package study.kstock.stockapi.config
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.cache.CacheManager
+import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.cache.RedisCacheConfiguration
-import org.springframework.data.redis.cache.RedisCacheManager
+import org.springframework.data.redis.cache.RedisCacheManager.RedisCacheManagerBuilder
 import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
 import org.springframework.data.redis.serializer.RedisSerializationContext
-import org.springframework.data.redis.serializer.StringRedisSerializer
 import java.time.Duration
+import javax.annotation.Resource
 
 @Configuration
+@EnableCaching
 class CacheConfig {
 
-    @Autowired
-    private lateinit var connectionFactory: RedisConnectionFactory
+    @Resource
+    private lateinit var redisConnectionFactory: RedisConnectionFactory
 
     @Bean
-    fun redisCacheManager(): CacheManager {
-        val redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
-            .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(StringRedisSerializer()))
-            .serializeValuesWith(
-                RedisSerializationContext.SerializationPair.fromSerializer(
-                    GenericJackson2JsonRedisSerializer()
-                )
-            )
-            .entryTtl(Duration.ofSeconds(30))
-        return RedisCacheManager.RedisCacheManagerBuilder
-            .fromConnectionFactory(connectionFactory)
-            .cacheDefaults(redisCacheConfiguration)
-            .build()
+    fun redisCacheManager(): CacheManager? {
+        val builder = RedisCacheManagerBuilder.fromConnectionFactory(redisConnectionFactory)
+        val redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig().serializeValuesWith(
+            RedisSerializationContext.SerializationPair.fromSerializer(GenericJackson2JsonRedisSerializer())
+        ).entryTtl(
+            Duration.ofSeconds(30)
+        )
+        builder.cacheDefaults(redisCacheConfiguration)
+        return builder.build()
     }
 }
