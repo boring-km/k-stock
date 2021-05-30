a783a8aa29f0058fa854d4f079fda5e2b418bc67
boring-km <kms0644804@naver.com>
2021-05-30 22:07:41 +0900

    [KS-APP-04] retrofit 클라이언트의 타임아웃 설정

--- android/app/src/main/java/study/kstockapp/network/RetrofitClient.kt
+++ android/app/src/main/java/study/kstockapp/network/RetrofitClient.kt
@@ -1,7 +1,9 @@
 package study.kstockapp.network
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
     private var instance: Retrofit? = null
@@ -12,6 +14,11 @@ object RetrofitClient {
             instance = Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .client(OkHttpClient.Builder()
+                        .connectTimeout(1, TimeUnit.MINUTES)
+                        .readTimeout(30, TimeUnit.SECONDS)
+                        .writeTimeout(30, TimeUnit.SECONDS)
+                        .build())
                     .build()
         }
 
